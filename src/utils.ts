ef4f026aa88f6797cea1126e93dd92a1b07eb932
iamacook <aaron.cook@gnosis.pm>
2022-05-20 11:18:39 +0200

    fix: compare deployment versions semantically

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,9 @@
 import { DeploymentFilter, SingletonDeployment } from "./types"
+import semverSatisfies from "semver/functions/satisfies";
 
 export const findDeployment = (criteria: DeploymentFilter, deployments: SingletonDeployment[]): SingletonDeployment | undefined =>
-    deployments.find((deployment) => {
-        if (criteria.version && deployment.version !== criteria.version) return false
+  deployments.find((deployment) => {
+        if (criteria.version && !semverSatisfies(deployment.version, criteria.version)) return false
         if (criteria.released && deployment.released != criteria.released) return false
         if (criteria.network && !deployment.networkAddresses[criteria.network]) return false
         return true
