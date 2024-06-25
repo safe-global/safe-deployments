242f6f828e23a20adbf50bc747bd6ed6d68f4191
Mikhail <16622558+mmv08@users.noreply.github.com>
2024-06-25 15:49:27 +0200

    migration script wip

--- src/jsonMigrationScript130.ts
+++ src/jsonMigrationScript130.ts
@@ -1,6 +1,10 @@
 import fs from "fs"
 import { SingletonDeploymentJSON } from "./types"
 
+function isHex(value: string): boolean {
+  return /^0x[0-9a-fA-F]*$/.test(value)
+}
+
 const fileList: string[] = [
   "compatibility_fallback_handler.json",
   "create_call.json",
@@ -20,7 +24,7 @@ for (const fileName of fileList) {
   const addresses = {
     canonical: json.networkAddresses[1],
     eip155: json.networkAddresses[10],
-    zksync: json.networkAddresses[321],
+    zksync: json.networkAddresses[324],
   }
 
   const newJson = {
@@ -36,49 +40,80 @@ for (const fileName of fileList) {
   }
 
   for (const entry of Object.entries(json.networkAddresses)) {
-    const [chainId] = entry
+    const [chainId, previousAddress] = entry
 
     const chainListUrl = `https://raw.githubusercontent.com/ethereum-lists/chains/master/_data/chains/eip155-${chainId}.json`
 
-    const chainData = (await fetch(chainListUrl).then((res) => res.json())) as { rpc: string[] }
-    console.log({ chainData })
+    const chainData = (await fetch(chainListUrl)
+      .then((res) => res.json())
+      .catch((e) => {
+        console.log(`Error fetching chain data for chain ${chainId}. Skipping...`, e)
+        return { rpc: [] }
+      })) as { rpc: string[] }
 
-    const rpc = chainData.rpc.find(
+    const rpcs = chainData.rpc.filter(
       (rpc) => !rpc.startsWith("ws") && !rpc.includes("INFURA_API_KEY")
     )
-    if (!rpc) {
-      throw new Error("RPC not found")
-    }
-
-    const networkAddresses: string[] = []
-    for (const value of Object.entries(addresses)) {
-      const [addrType, addr] = value
-      const deployedCode = await fetch(rpc, {
-        method: "POST",
-        body: JSON.stringify({
-          jsonrpc: "2.0",
-          method: "eth_getCode",
-          params: [addr, "latest"],
-          id: 1,
-        }),
-      }).then((res) => res.json() as any)
-
-      if (deployedCode.result === "0x" || deployedCode.result === "") {
-        console.log(`Contract ${addr} not deployed on chain ${chainId}. Skipping...`)
-      } else {
-        networkAddresses.push(addrType)
-      }
-    }
-
-    if (networkAddresses.length === 0) {
-      console.log(`Contract ${fileName} not deployed on chain ${chainId}. Skipping...`)
+    if (!rpcs.length) {
+      console.log(`Chain ${chainId} not supported. Copying previous value...`)
+      newJson.networkAddresses[chainId] = previousAddress
       continue
-    } else if (networkAddresses.length === 1) {
-      newJson.networkAddresses[chainId] = networkAddresses[0]
-    } else {
-      newJson.networkAddresses[chainId] = networkAddresses
     }
+
+    for (const rpc of rpcs) {
+      const networkAddresses: string[] = []
+      for (const value of Object.entries(addresses)) {
+        const [addrType, addr] = value
+        const deployedCode = await fetch(rpc, {
+          method: "POST",
+          body: JSON.stringify({
+            jsonrpc: "2.0",
+            method: "eth_getCode",
+            params: [addr, "latest"],
+            id: 1,
+          }),
+        })
+          .then((res) => {
+            const json = res.json()
+
+            if ("result" in json) {
+              return json
+            }
+
+            throw new Error("Invalid response")
+          })
+          .catch((e) => {
+            console.log(`Error fetching code for ${addr} on chain ${chainId}. Skipping...`, e)
+            return { result: "" }
+          })
+
+        if (deployedCode.result === "0x" || deployedCode.result === "") {
+          console.log(`Contract ${addr} not deployed on chain ${chainId}. Skipping...`)
+          continue
+        } else {
+          networkAddresses.push(addrType)
+        }
+      }
+
+      if (networkAddresses.length === 0) {
+        console.log(
+          `Contract ${fileName} not deployed on chain ${chainId}. Fallback to old json...`
+        )
+        newJson.networkAddresses[chainId] = previousAddress
+        continue
+      } else if (networkAddresses.length === 1) {
+        newJson.networkAddresses[chainId] = networkAddresses[0]
+      } else {
+        newJson.networkAddresses[chainId] = networkAddresses
+      }
+
+      break
+    }
+
+    break
   }
 
-  fs.writeFileSync(`./src/assets/new/v1.3.0/${fileName}`, JSON.stringify(newJson, null, 2))
+  fs.writeFileSync(`./src/assets/v1.3.0/new-${fileName}`, JSON.stringify(newJson, null, 2))
+
+  break
 }
