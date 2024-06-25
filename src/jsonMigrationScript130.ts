82bafc020c43fb1ccb5b4c02fe24e3f4be12f7fc
Mikhail <16622558+mmv08@users.noreply.github.com>
2024-06-25 13:10:43 +0200

    migration script wip

---
+++ src/jsonMigrationScript130.ts
@@ -0,0 +1,84 @@
+import fs from "fs"
+import { SingletonDeploymentJSON } from "./types"
+
+const fileList: string[] = [
+  "compatibility_fallback_handler.json",
+  "create_call.json",
+  "gnosis_safe_l2.json",
+  "gnosis_safe.json",
+  "multi_send.json",
+  "multi_send_call_only.json",
+  "proxy_factory.json",
+  "sign_message_lib.json",
+  "simulate_tx_accessor.json",
+]
+
+for (const fileName of fileList) {
+  const file = fs.readFileSync(`./src/assets/v1.3.0/${fileName}`, "utf8")
+  const json = JSON.parse(file) as SingletonDeploymentJSON
+
+  const addresses = {
+    canonical: json.networkAddresses[1],
+    eip155: json.networkAddresses[10],
+    zksync: json.networkAddresses[321],
+  }
+
+  const newJson = {
+    ...json,
+    addresses,
+    networkAddresses: {} as Record<string, string | string[]>,
+  }
+
+  for (const value of Object.values(addresses)) {
+    if (!value) {
+      throw new Error("Address not found")
+    }
+  }
+
+  for (const entry of Object.entries(json.networkAddresses)) {
+    const [chainId] = entry
+
+    const chainListUrl = `https://raw.githubusercontent.com/ethereum-lists/chains/master/_data/chains/eip155-${chainId}.json`
+
+    const chainData = (await fetch(chainListUrl).then((res) => res.json())) as { rpc: string[] }
+    console.log({ chainData })
+
+    const rpc = chainData.rpc.find(
+      (rpc) => !rpc.startsWith("ws") && !rpc.includes("INFURA_API_KEY")
+    )
+    if (!rpc) {
+      throw new Error("RPC not found")
+    }
+
+    const networkAddresses: string[] = []
+    for (const value of Object.entries(addresses)) {
+      const [addrType, addr] = value
+      const deployedCode = await fetch(rpc, {
+        method: "POST",
+        body: JSON.stringify({
+          jsonrpc: "2.0",
+          method: "eth_getCode",
+          params: [addr, "latest"],
+          id: 1,
+        }),
+      }).then((res) => res.json() as any)
+
+      if (deployedCode.result === "0x" || deployedCode.result === "") {
+        console.log(`Contract ${addr} not deployed on chain ${chainId}. Skipping...`)
+      } else {
+        networkAddresses.push(addrType)
+      }
+    }
+
+    if (networkAddresses.length === 0) {
+      console.log(`Contract ${fileName} not deployed on chain ${chainId}. Skipping...`)
+      continue
+    } else if (networkAddresses.length === 1) {
+      newJson.networkAddresses[chainId] = networkAddresses[0]
+    } else {
+      newJson.networkAddresses[chainId] = networkAddresses
+    }
+  }
+
+  fs.writeFileSync(`./src/assets/new/v1.3.0/${fileName}`, JSON.stringify(newJson, null, 2))
+}
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,12 @@
+export interface SingletonDeploymentJSON {
+    released: boolean
+    contractName: string,
+    version: string,
+    codeHash: string,
+    networkAddresses: Record<string, string>,
+    abi: any[],
+}
+
 export interface SingletonDeployment {
     defaultAddress: string,
     released: boolean
