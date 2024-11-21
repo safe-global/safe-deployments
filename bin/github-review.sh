2a7811b127874ef43b7949f6b5d7045ff6ea2db3
Shebin John <admin@remedcu.com>
2024-11-21 15:45:00 +0100

    RPC Taken from `ethereum-lists` directly (#873)
    Solved #688 
    
    This pull request includes a small but significant change to the
    `.github/PULL_REQUEST_TEMPLATE.md` file. The change updates the
    instructions for adding a new chain by specifying that the RPC URL will
    be taken from the `ethereum-lists/chains` repository.
    
    *
    [`.github/PULL_REQUEST_TEMPLATE.md`](diffhunk://#diff-18813c86948efc57e661623d7ba48ff94325c9b5421ec9177f724922dd553a35L3-L12):
    Updated the instructions to clarify that the RPC URL will be sourced
    from the `ethereum-lists/chains` repository.
    * `bin/github-review.sh`: Updated how RPC is fetched (Before: from PR
    description. After: from `ethereum-lists`).

--- bin/github-review.sh
+++ bin/github-review.sh
@@ -45,9 +45,10 @@ if [[ -z $chainid ]]; then
     echo "ERROR: Chain ID not specified as per the PR Template" 1>&2
     exit 1
 fi
-rpc="$(gh pr view $pr | sed -nE 's|^- RPC_URL: (https?://[^ ]+).*$|\1|p')"
+chainInfo="https://raw.githubusercontent.com/ethereum-lists/chains/refs/heads/master/_data/chains/eip155-$chainid.json"  
+rpc="$(curl -sfL "$chainInfo" | jq -r '.rpc[0]')" 
 if [[ -z $rpc ]]; then
-    echo "ERROR: RPC not specified as per the PR Template" 1>&2
+    echo "ERROR: RPC not fetched correctly from the ethereum-lists" 1>&2
     exit 1
 fi
 version="$(gh pr diff $pr --name-only | sed -nE 's|^src/assets/v([0-9\.]*)/.*$|\1|p' | sort -u)"
